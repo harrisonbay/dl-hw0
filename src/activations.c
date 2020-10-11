9a76d8deb7b635507f2323a560b3153a2f3ae44d
arnavmdas <arnavdas2016@gmail.com>
2020-10-11 14:15:31 -0500

    Update activations.c

--- src/activations.c
+++ src/activations.c
@@ -12,20 +12,40 @@ void activate_matrix(matrix m, ACTIVATION a)
     for(i = 0; i < m.rows; ++i){
         double sum = 0;
         for(j = 0; j < m.cols; ++j){
-            double x = m.data[i*m.cols + j];
+            //double x = m.data[i*m.cols + j];
             if(a == LOGISTIC){
                 // TODO
+                m.data[i*m.cols + j] = 1/(1 + exp(-1*m.data[i*m.cols + j]));
+
             } else if (a == RELU){
                 // TODO
+                if (m.data[i*m.cols + j] >= 0) {
+                  m.data[i*m.cols + j] = m.data[i*m.cols + j];
+                } else {
+                  m.data[i*m.cols + j] = 0;
+                }
+
             } else if (a == LRELU){
                 // TODO
+                if (m.data[i*m.cols + j] >= 0) {
+                  m.data[i*m.cols + j] = m.data[i*m.cols + j];
+                } else {
+                  m.data[i*m.cols + j] = .001*m.data[i*m.cols + j];
+                }
+
             } else if (a == SOFTMAX){
                 // TODO
+                m.data[i*m.cols + j] = exp(m.data[i*m.cols + j]);
             }
+
             sum += m.data[i*m.cols + j];
         }
+
         if (a == SOFTMAX) {
             // TODO: have to normalize by sum if we are using SOFTMAX
+            for (int k = 0; k < m.cols; k++) {
+              m.data[i*m.cols + k] = m.data[i*m.cols + k]/(sum + .001);
+            }
         }
     }
 }
@@ -38,10 +58,28 @@ void activate_matrix(matrix m, ACTIVATION a)
 void gradient_matrix(matrix m, ACTIVATION a, matrix d)
 {
     int i, j;
+
     for(i = 0; i < m.rows; ++i){
         for(j = 0; j < m.cols; ++j){
             double x = m.data[i*m.cols + j];
             // TODO: multiply the correct element of d by the gradient
+            if (a == LOGISTIC) {
+              d.data[i*d.cols + j] *= x * (1 - x);
+            } else if (a == RELU) {
+                if (x > 0) {
+                  d.data[i*d.cols + j] *= 1;
+                } else {
+                  d.data[i*d.cols + j] *= 0;
+                }
+            } else if (a == LRELU) {
+                if (x > 0) {
+                  d.data[i*d.cols + j] *= 1;
+                } else {
+                  d.data[i*d.cols + j] *= 0.001;
+                }
+            } else if (a == SOFTMAX) {
+                d.data[i*d.cols + j] *= 1;
+            }
         }
     }
 }
